3547a286f631525711edb1d6bdbf84184a3880eb
Caroline Levison <caroline.levison@cloudinary.com>
2022-06-28 12:25:10 +0100

    Update code to use async functions.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require the cloudinary library
-var cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
 
 // Return "https" URLs by setting secure: true
 cloudinary.config({
@@ -9,44 +9,51 @@ cloudinary.config({
 // Log the configuration
 console.log(cloudinary.config());
 
+
 /////////////////////////
 // Uploads an image file
 /////////////////////////
-function uploadImage(callback)
-{
-    // Upload an image
-    cloudinary.uploader.upload("https://cloudinary-devs.github.io/cld-docs-assets/assets/images/happy_people.jpg", 
-        { 
-            // Use the uploaded file's name as the asset's public ID and allow overwriting the asset with new versions.
-            use_filename: true,
-            unique_filename: false,
-            overwrite: true
-        },
-        function(error, result) 
-        {
-            console.log(result, error);
-            callback(result.public_id); 
-        }
-    );
-}
+const uploadImage = async (imagePath) => {
+
+    // Use the uploaded file's name as the asset's public ID and 
+    // allow overwriting the asset with new versions
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+    };
+
+    try {
+      // Upload the image
+      const result = await cloudinary.uploader.upload(imagePath, options);
+      console.log(result);
+      return result.public_id;
+    } catch (error) {
+      console.error(error);
+    }
+};
+    
 
 /////////////////////////////////////
 // Gets details of an uploaded image
 /////////////////////////////////////
-function getAssetInfo(publicId, callback)
-{
-    // Get details of the uploaded image
-    cloudinary.api.resource(publicId, 
-        { 
-            colors: true 
-        },
-        function(error, result) 
-        {
-            console.log(result, error); 
-            callback(result);
-        }
-    );
-}
+const getAssetInfo = async (publicId) => {
+
+    // Return colors in the response
+    const options = {
+      colors: true,
+    };
+
+    try {
+        // Get details about the asset
+        const result = await cloudinary.api.resource(publicId, options);
+        console.log(result);
+        return result.colors;
+        } catch (error) {
+        console.error(error);
+    }
+};
+   
 
 //////////////////////////////////////////////////////////////
 // Creates an HTML image tag with a transformation that
@@ -54,29 +61,45 @@ function getAssetInfo(publicId, callback)
 // focused on the faces, applying an outline of the  
 // first color, and setting a background of the second color.
 //////////////////////////////////////////////////////////////
-function createImageTag(publicId, color1, color2)
-{
-    // Create an image tag with transformations applied to the src URL.
-    let imageTag = cloudinary.image(publicId, 
-        {
-            transformation: [
-                { width: 250, height: 250, gravity: "faces", crop: "thumb" },
-                { radius: "max" },
-                { effect: "outline:10", color: color1 },
-                { background: color2 }
-            ]
-        }
-    );
+const createImageTag = (publicId, ...colors) => {
     
+    // Set the effect color and background color
+    const [effectColor, backgroundColor] = colors;
+
+    // Create an image tag with transformations applied to the src URL
+    let imageTag = cloudinary.image(publicId, {
+      transformation: [
+        { width: 250, height: 250, gravity: 'faces', crop: 'thumb' },
+        { radius: 'max' },
+        { effect: 'outline:10', color: effectColor },
+        { background: backgroundColor },
+      ],
+    });
+
+    return imageTag;
+};
+   
+
+//////////////////
+//
+// Main function
+//
+//////////////////
+(async () => {
+
+    // Set the image to upload
+    const imagePath = 'https://cloudinary-devs.github.io/cld-docs-assets/assets/images/happy_people.jpg';
+
+    // Upload the image
+    const publicId = await uploadImage(imagePath);
+
+    // Get the colors in the image
+    const colors = await getAssetInfo(publicId);
+
+    // Create an image tag, using two of the colors in a transformation
+    const imageTag = await createImageTag(publicId, colors[0][0], colors[1][0]);
+
     // Log the image tag to the console
     console.log(imageTag);
-}
 
-// Upload an image
-uploadImage(function(publicId) {
-    // Get information about the image
-    getAssetInfo(publicId, function(result){
-        // Create an image tag, using two of the colors in a transformation
-        createImageTag(publicId, result.colors[0][0], result.colors[1][0]);
-    });
-});
+})();
